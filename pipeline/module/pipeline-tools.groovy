b5cf03723f8f99aa72f9b8873045ddd10bc16117
sunjun <sunjun2@hotmail.com>
2019-08-15 23:04:23 +0800

    init 2

---
+++ pipeline/module/pipeline-tools.groovy
@@ -0,0 +1,10 @@
+import hudson.model.*;
+import groovy.json.*;
+
+def find_files(filetype) {
+
+	def files = findFiles(glob:filetype)
+	for (file in files) {
+		println file.name
+	}
+}
--- pipeline/src/pipeline-script-job.groovy
+++ pipeline/src/pipeline-script-job.groovy
@@ -6,12 +6,13 @@ println env.BUILD_NUMBER
 println env.WORKSPACE
 
 pipeline {
-
     agent any
     stages {
         stage("init") {
             steps {
                 script {
+                    model_test = load env.WORKSPACE + "/pipeline/module/pipeline-tools.groovy"
+                    println model_test
                     println "欢迎来到Anthony的Pipeline教程。"
                 }
             }
